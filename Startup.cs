3c35ee57699a8bfb6a34eafe7ae52f7a901ee9ec
fleetingold <1161851523@qq.com>
2021-05-19 20:20:01 +0800

    UseSwagger

--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +27,18 @@ namespace FleetingoldWebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            #region ConfigureServices -> Swagger Code
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo 
+                {
+                    Version = "v1.0.0",
+                    Title = "Fleetingold WebApi",
+                    Description = "Fleetingold ¿ª·ÅAPI" 
+                });
+            });
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +47,11 @@ namespace FleetingoldWebAPI
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+
+                #region IApplicationBuilder -> Swagger Code
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApi v1"));
+                #endregion
             }
 
             app.UseHttpsRedirection();
